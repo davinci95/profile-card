4b2744ab686b2eb2c3b7b80fac51049f5ab511c7
davinci95 <vodinhvan200495@gmail.com>
2022-11-29 12:06:49 +0700

    Updates

---
+++ precache-manifest.d4733075439e71ec20569ca22bd71325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "653b5f17c92ce9c90dc2",
+    "url": "/profile-card/static/css/main.6a0ca2b3.chunk.css"
+  },
+  {
+    "revision": "653b5f17c92ce9c90dc2",
+    "url": "/profile-card/static/js/main.653b5f17.chunk.js"
+  },
+  {
+    "revision": "0ce75190d20f6dd07c95",
+    "url": "/profile-card/static/js/1.0ce75190.chunk.js"
+  },
+  {
+    "revision": "fb0db163b08e523be127",
+    "url": "/profile-card/static/js/runtime~main.fb0db163.js"
+  },
+  {
+    "revision": "c356a1ecc9e6bdf90281fbc1459d729d",
+    "url": "/profile-card/static/media/cat.c356a1ec.jpg"
+  },
+  {
+    "revision": "36403c5b6921e8edef85309608c6d135",
+    "url": "/profile-card/index.html"
+  }
+];
